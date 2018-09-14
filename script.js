6f9266cfc307e2c73a6754a91cfa7bf2d064138e
Ali Karbassi <ali@karbassi.com>
2018-09-13 21:23:15 -0500

    Step 8

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ console.log(age * 5);
 var last_name = "Karbassi";
 console.log(name + " " + last_name);
 var fruits = [];
+fruits.push(
+    "Apples",
+    "Oranges",
+    "Pears",
+    "Bananas",
+    "Cherries",
+);
